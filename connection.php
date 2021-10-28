9d0e774a7a800b59e3d6bff8f4ac35e8251b6c5a
alaasaadallah <alaa.saadallah96@gmail.com>
2021-10-28 20:53:50 +0200

    connected to database

--- connection.php
+++ connection.php
@@ -1,3 +1,25 @@
 <?php
-echo "this is connection file";
+// echo "this is connection file";
+$servername = "localhost";
+$username = "root";
+$password = "root";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}else{
+    echo "connected successfully";
+}
+
+// Create database
+$sql = "CREATE DATABASE shipping";
+if ($conn->query($sql) === TRUE) {
+//   echo "Database created successfully";
+} else {
+//   die("Error creating database: " . $conn->error);
+}
+
+$conn->close();
 ?>
